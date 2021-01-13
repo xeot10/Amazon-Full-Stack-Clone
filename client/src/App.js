a8f9a1764544421f5c2db7f8bfe679aef22f848a
xeot10 <59329844+xeot10@users.noreply.github.com>
2021-01-14 03:12:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Header from './header';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Amazon Full Stack Clone</h1>
-      </header>
+      <Header />
     </div>
   );
 }
